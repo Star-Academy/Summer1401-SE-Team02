cfdc34273b74a5c96c594b1501e8cecb0fc42e3a
Sajad <s.soltaniant@gmail.com>
2022-07-17 15:09:35 +0430

    cleaning Main class

--- Main.java
+++ Main.java
@@ -8,16 +8,23 @@ public class Main {
      }
 
      private static void runProgram(){
+          loadData();
+          getQueries();
+     }
+
+     
+     private static void getQueries(){
+          Scanner scanner = new Scanner(System.in);
+          String word;
+          while (!(word = scanner.nextLine()).equals("-1")){
+               System.out.println(SearchEngine.search(word));
+          }
+     }
+     private static void loadData(){
           try {
                loadFiles();               
           } catch (Exception e) {
                System.out.println(e);
-          }
-
-          Scanner scanner = new Scanner(System.in);
-          String word;
-          while ((word = scanner.nextLine()).equals("-1")){
-               System.out.println(SearchEngine.search(word));
           }
      }
 
