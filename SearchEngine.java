6cf23f8a13a0f12e007a4deb0b2750d83c5e0eac
Soltaniant <47256432+Soltaniant@users.noreply.github.com>
2022-07-17 12:16:22 +0430

    add seach engine main structure

---
+++ SearchEngine.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+class SearchEngine{
+     private static HashMap<String, ArrayList> indexedData;
+     
+     static {
+          indexedData = new HashMap<>();
+     }
+
+     public static ArrayList<String> search(String word){
+          return null;
+     }
+
+     public static void addFile(String text, String docID){
+          //TODO tokenize the text and add each word to our indexed database
+     }
+
+     private static ArrayList<String> tokenize(String text){
+          //TODO: tokenize the text and seperate it to words. (most simple way)
+          return null;
+     }
+
+     private static void addWord(String word, String docID){
+          //TODO: check whether the word is new or not. if its new, simply add it to the indexedData, else, insert the docID to the list.
+          
+     }
+
+     private static void insertDocID(String word, ArrayList<String> docsIDs, String DocID){
+          //TODO: insert the docID to the list and replace it in indexedData.
+     }
+}
