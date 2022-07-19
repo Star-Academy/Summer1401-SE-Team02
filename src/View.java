468b7ce60f9002abd3e24e5140e08b4e81e4bcfc
Sajad <s.soltaniant@gmail.com>
2022-07-19 17:54:42 +0430

    add error and success message

--- src/View.java
+++ src/View.java
@@ -2,7 +2,10 @@ package src;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import src.enums.ColorCodes;
+
 public class View {
+ 
      private static final Scanner scanner;
 
      static {
@@ -19,8 +22,17 @@ public class View {
      }
 
      private static void showOutput(ArrayList<String> output){
-          System.out.println(output);
+          if (output.isEmpty()) _error("Oops! no result ...");
+          else _success(output);
      }
 
+     private static void _error(String message){
+          System.out.println(ColorCodes.RED_BOLD.getCode() + "! " + ColorCodes.ANSI_RESET.getCode() + message);
+     }
+
+     private static void _success(ArrayList<String> output){
+          System.out.println(String.format( "%s+ %snumber of search results: %d\n%s",
+           ColorCodes.GREEN_BOLD.getCode(), ColorCodes.ANSI_RESET.getCode(), output.size(), output));
+     }
 
 }
---
+++ src/enums/ColorCodes.java
@@ -0,0 +1,15 @@
+package src.enums;
+
+public enum ColorCodes {
+     ANSI_RESET ("\u001B[0m"),
+     RED_BOLD ("\033[1;31m"),
+     GREEN_BOLD("\033[1;32m");  // GREEN
+
+     private final String code;
+
+     ColorCodes(String code){
+          this.code = code;
+     }
+
+     public String getCode(){return this.code;}
+}
