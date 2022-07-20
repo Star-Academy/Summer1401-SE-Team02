1c4df51818d91987d2ea478d81d49fcb13b56eb0
Sajad <s.soltaniant@gmail.com>
2022-07-20 12:28:08 +0430

    refactoring ... (normalizer static field was replaced)

--- src/Normalizer.java
+++ src/Normalizer.java
@@ -3,22 +3,15 @@ package src;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import src.enums.Constants;
+
 public class Normalizer {
 
-    static private final ArrayList<String> stopWords = new ArrayList<String>(Arrays.asList("OURSELVES", "HERS",
-            "BETWEEN", "YOURSELF", "BUT", "AGAIN", "THERE", "ABOUT", "ONCE", "DURING", "OUT", "VERY", "HAVING", "WITH",
-            "THEY", "OWN", "AN", "BE", "SOME", "FOR", "DO", "ITS", "YOURS", "SUCH", "INTO", "OF", "MOST", "ITSELF",
-            "OTHER", "OFF", "IS", "S", "AM", "OR", "WHO", "AS", "FROM", "HIM", "EACH", "THE", "THEMSELVES", "UNTIL",
-            "BELOW", "ARE", "WE", "THESE", "YOUR", "HIS", "THROUGH", "DON", "NOR", "ME", "WERE", "HER", "MORE",
-            "HIMSELF", "THIS", "DOWN", "SHOULD", "OUR", "THEIR", "WHILE", "ABOVE", "BOTH", "UP", "TO", "OURS", "HAD",
-            "SHE", "ALL", "NO", "WHEN", "AT", "ANY", "BEFORE", "THEM", "SAME", "AND", "BEEN", "HAVE", "IN", "WILL",
-            "ON", "DOES", "YOURSELVES", "THEN", "THAT", "BECAUSE", "WHAT", "OVER", "WHY", "SO", "CAN", "DID", "NOT",
-            "NOW", "UNDER", "HE", "YOU", "HERSELF", "HAS", "JUST", "WHERE", "TOO", "ONLY", "MYSELF", "WHICH", "THOSE",
-            "I", "AFTER", "FEW", "WHOM", "T", "BEING", "IF", "THEIRS", "MY", "AGAINST", "A", "BY", "DOING", "IT", "HOW",
-            "FURTHER", "WAS", "HERE", "THAN"));
-
     public static ArrayList<String> normalize(String text) {
-        ArrayList<String> result = removeStopWords(tokenize(refine(text)));
+        ArrayList<String> result = removeStopWords(tokenize(refine(text)),
+                new ArrayList<String>(
+                        Arrays.asList(
+                                Constants.STOPPING_WORDS.toString().toUpperCase().split(Constants.SPACE.toString()))));
         return result;
     }
 
@@ -33,7 +26,7 @@ public class Normalizer {
         return text.replaceAll("[^a-zA-Z ]", "").toUpperCase();
     }
 
-    private static ArrayList<String> removeStopWords(ArrayList<String> words) {
+    private static ArrayList<String> removeStopWords(ArrayList<String> words, ArrayList<String> stopWords) {
         words.removeAll(stopWords);
         return words;
     }
--- src/enums/Constants.java
+++ src/enums/Constants.java
@@ -6,7 +6,12 @@ public enum Constants {
      SIMPLE_WORDS_REGEX("(?<!\\S)"),
      EXTRACT_WORD_REGEX("(\\w+)"),
 
-     END_OF_PROGRAM("-1");
+     SPACE("\\s+"),
+
+     END_OF_PROGRAM("-1"),
+
+     STOPPING_WORDS(
+               "a about above across after afterwards again against all almost alone along also although always am among amount an and another any anyhow anyone anything anyway anywhere are around as at back be became because become becomes been being below beside besides between beyond both but by came can cant cannot come could couldnt describe did didnt do does doesnt doing done dont due during each either else elsewhere enough etc even ever every everyone everything everywhere few for from further get give goes going had happen has hasnt have having here how however i if ill im in into is isnt it its ive just keep let like made make many may me mean might mine more most mostly much name next no nobody not nothing now of once only onto or other others otherwise our over per perhaps please put rather re really same say see seem seemed seeming seems several should show side since so some somehow someone something sometime sometimes somewhere still such take tell than that the then their them then there these they thing this those through throughout to together too try un up upon us use used uses very want was way we well were what whatever when where wherever whether which who whoever whole whom whose why will with within without wont would you your youre yours yourself");
 
      private final String value;
 
