9367ba909a0d10bc300f55da46d301eef5bc7b11
Parsa-Sh-Y <p.shafiee.yekta@gmail.com>
2022-07-20 11:52:04 +0430

    Refactored OrderedSet Class

--- src/OrderedSet.java
+++ src/OrderedSet.java
@@ -1,29 +1,32 @@
 package src;
+
 import java.util.ArrayList;
 
 public class OrderedSet {
-     
-     public static ArrayList<Integer> intersection(ArrayList<Integer> a, ArrayList<Integer> b){
+
+     public static ArrayList<Integer> intersection(ArrayList<Integer> a, ArrayList<Integer> b) {
           ArrayList<Integer> result = (ArrayList<Integer>) a.clone();
           result.retainAll(b);
           return result;
      }
-     public static ArrayList<Integer> union(ArrayList<Integer> a, ArrayList<Integer> b){
+
+     public static ArrayList<Integer> union(ArrayList<Integer> a, ArrayList<Integer> b) {
           ArrayList<Integer> result = new ArrayList<Integer>();
           result.addAll(a);
           result.addAll(b);
           for (Integer e : intersection(a, b)) {
-              result.remove(e);
+               result.remove(e);
           }
           return result;
      }
-     public static ArrayList<Integer> subtract(ArrayList<Integer> a, ArrayList<Integer> b){
+
+     public static ArrayList<Integer> subtract(ArrayList<Integer> a, ArrayList<Integer> b) {
           ArrayList<Integer> result = new ArrayList<Integer>();
           result.addAll(a);
           for (Integer e : intersection(a, b)) {
-              result.remove(e);
+               result.remove(e);
           }
           return result;
      }
-     
+
 }
